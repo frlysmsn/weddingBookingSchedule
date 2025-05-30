56b848b5ee2615e220160bc89e94c0ef7480ec96
LyAnn <ferlyann.samson@evsu.edu.ph>
2025-05-30 22:23:44 +0800

    setup_admin.php

--- setup_admin.php
+++ setup_admin.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'includes/config.php';
-require_once 'includes/db_connection.php';
+require_once 'includes/db_connection.php'; 
 
 try {
     $db = Database::getInstance()->getConnection();
