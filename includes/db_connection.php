2b266b9db29a53a6707ffdbb18ddf9fd9902448b
LyAnn <ferlyann.samson@evsu.edu.ph>
2025-05-30 22:52:10 +0800

    db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -6,7 +6,7 @@ class Database {
     private $conn;
 
     private function __construct() {
-        try {
+        try {  
             $this->conn = new PDO(
                 "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
                 DB_USER,
