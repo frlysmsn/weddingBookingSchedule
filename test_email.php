76dc17d0197e220cc1e1b00ab7bcf3cfa4dac21c
LyAnn <ferlyann.samson@evsu.edu.ph>
2025-05-30 22:20:23 +0800

    test_email.php

--- test_email.php
+++ test_email.php
@@ -4,7 +4,7 @@ require_once 'includes/Mail.php';
 $mailer = new Mail();
 $success = $mailer->sendBookingApproval('test@example.com', [
     'wedding_date' => '2023-12-25',
-    'preferred_time' => '10:00 AM'
+    'preferred_time' => '10:00 AM' 
 ]);
 
 if ($success) {
