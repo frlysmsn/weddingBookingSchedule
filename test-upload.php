0b490d33443e76e4b1aa1b08c1ca72e9358e3b77
LyAnn <ferlyann.samson@evsu.edu.ph>
2025-05-30 22:22:57 +0800

    test-upload.php

--- test-upload.php
+++ test-upload.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Simulate a logged-in user if needed 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) { 
     $_SESSION['user_id'] = 1; // Replace with a valid user ID
 }
 
