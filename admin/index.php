06e2cfed2fa0bc3bcb43da2cbd34240d3b73704b
LyAnn <ferlyann.samson@evsu.edu.ph>
2025-05-30 22:43:15 +0800

    index.php

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     header('Location: ../index.php');
     exit;
 }
-
+ 
 // Get pending counts
 $db = Database::getInstance()->getConnection();
 
